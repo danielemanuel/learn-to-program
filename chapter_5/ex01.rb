4b6a2ce62efaba0681384c09fec19aaa54d9b74e
danielemanuel <daniel.costea@vkernel.ro>
2017-03-20 12:34:26 +0000

    chapter_5 finish

--- chapter_5/ex01.rb
+++ chapter_5/ex01.rb
@@ -1,8 +1,21 @@
+Result :
+
 puts "What's your name?"
 f_name = gets.chomp
 puts "What's your middle name ?"
 m_name = gets.chomp
-puts "What's your last name"
+puts "What's your last name?"
 l_name = gets.chomp
 
-puts "Your full name is #{f_name} #{m_name} #{l_name}."
+puts "Hi there #{f_name} #{m_name} #{l_name}, nice to meet you budy!"
+
+Result: 
+
+What's your name?
+ Daniel
+What's your middle name ?
+ Emanuel
+What's your last name?
+ Costea
+Hi there Daniel Emanuel Costea, nice to meet you budy.
+=> nil
---
+++ chapter_5/ex02.rb
@@ -0,0 +1,11 @@
+puts "What's your favourite number ?"
+f_number = gets.chomp.to_i
+result = f_number + 1
+puts "Your number incremented to number 1 is number #{result}"
+
+Result :
+
+What's your favourite number ?
+ 10
+Your number incremented by number 1 is number 11
+=> nil
