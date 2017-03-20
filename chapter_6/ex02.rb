71a028e7d522fd5e95fb34692a96ab67f443c2ac
danielemanuel <daniel.costea@vkernel.ro>
2017-03-20 13:39:14 +0000

    chapter_6

---
+++ chapter_6/ex02.rb
@@ -0,0 +1,16 @@
+title = 'Table of contents'
+line_width = 40
+
+puts(title.center(line_width))
+puts
+puts('Chapter_1: Getting Started'.ljust(30) + 'page_1'.rjust(line_width/ 2))
+puts('Chapter_2: Numbers'.ljust(30) + 'page_9'.rjust(line_width/ 2))
+puts('Chapter_3: Letters'.ljust(31) + 'page_13'.rjust(line_width/ 2))
+
+Result: 
+
+           Table of contents            
+
+Chapter_1: Getting Started                  page_1
+Chapter_2: Numbers                          page_9
+Chapter_3: Letters                          page_13
